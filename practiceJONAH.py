ac9d1232d7751f383d3b4d71cd1b423952d7da25
Jonah Song <jonahsong@Jonahs-MacBook-Pro.local>
2020-02-26 10:59:59 -0800

    getnRandom(n) function added

--- practiceJONAH.py
+++ practiceJONAH.py
@@ -2,13 +2,19 @@
 # Partner 2: Isabelle Torch
 ##############################
 # Assignment Name: GitHub Practice (2/25/20) - 20 Points
+##############################
+import random
 
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+    '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+    n = int(input("Input an integer. "))   
+    numbers = []
+    for i in range(n):
+        numbers.append(random.randint(1,10))
+    return numbers
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
+    '''takes in a list of n numbers and returns the product of the numbers'''
     pass
 
 def main():
